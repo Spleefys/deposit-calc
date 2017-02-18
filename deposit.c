2a884140d599e9b225731376dfaa3ce8d1aea311
Гурулев Дмитрий Александрович <iv621s05@awp10.csc.local>
2017-02-18 12:13:22 +0700

    Vvod dannih

---
+++ deposit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int sum, time;
+    
+    printf("Введите сумму вклада:");
+    scanf("%d", &sum);
+    printf("Введите срок:");
+    scanf("%d", &sum);
+    
+    return 0;
+}
