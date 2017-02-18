71b5fcd172e4350d78d2883fd34e3661b7b99993
Гурулев Дмитрий Александрович <iv621s05@awp10.csc.local>
2017-02-18 13:00:40 +0700

    Count deposit

--- deposit.c
+++ deposit.c
@@ -10,7 +10,7 @@ void check(int sum, int time, int *n){
 }
 
 int main(){
-    int sum, n, time;
+    int sum, n, time, res;
     
     n = 0;
     
@@ -22,6 +22,43 @@ int main(){
     scanf("%d", &time);
     
     check(sum, time, &n);}
+    
+    if(sum<100000){
+        if(time > 0 && time <= 30){
+            res=sum-(sum*0.1);
+            printf("%d", res);
+        }
+        if(time >= 31 && time <= 120){
+            res=sum+(sum*0.02);
+            printf("%d", res);
+        }
+        if(time >= 121 && time <= 240){
+            res=sum+(sum*0.06);
+            printf("%d", res);
+        }
+        if(time >= 241 && time <= 365){
+            res=sum+(sum*0.12);
+            printf("%d", res);
+        }
+    }
+    else{
+        if(time > 0 && time <= 30){
+            res=sum-(sum*0.1);
+            printf("%d", res);
+        }
+        if(time >= 31 && time <= 120){
+            res=sum+(sum*0.03);
+            printf("%d", res);
+        }
+        if(time >= 121 && time <= 240){
+            res=sum-(sum*0.08);
+            printf("%d", res);
+        }
+        if(time >= 241 && time <= 365){
+            res=sum-(sum*0.15);
+            printf("%d", res);
+        }
+    }
     
     return 0;
 }
