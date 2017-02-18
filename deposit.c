edc7c2ab6202d428a8cbae5a83b186ceb17b0066
Гурулев Дмитрий Александрович <iv621s05@awp10.csc.local>
2017-02-18 12:39:48 +0700

    Check values

--- deposit.c
+++ deposit.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 
+void check(int sum, int time, int *n){
+    if (time > 365 && sum < 10000){
+        *n = 0;
+    }
+    else{
+        *n = 1;
+    }
+}
+
 int main(){
-    int sum, time;
+    int sum, n, time;
     
-    printf("Введите сумму вклада:");
+    n = 0;
+    
+    while (n == 0){
+    
+    printf("\nВведите сумму вклада:");
     scanf("%d", &sum);
     printf("Введите срок:");
-    scanf("%d", &sum);
+    scanf("%d", &time);
+    
+    check(sum, time, &n);}
     
     return 0;
 }
